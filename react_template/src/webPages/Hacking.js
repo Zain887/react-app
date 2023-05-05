61178e37969e535d56606d5963059a63a6c8e68d
zain <zain@parseclabs.ca>
2023-05-05 15:08:17 +0500

    remove directory reacte-tamplet

--- react_template/src/webPages/Hacking.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Hacking = () => {
-  return (
-    <div>Hacking</div>
-  )
-}
-
-export default Hacking
--- react_template/src/webPages/Seminar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Seminar = () => {
-  return (
-    <div>Seminar</div>
-  )
-}
-
-export default Seminar
